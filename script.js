ab3c3d118b8760f7459954b227c5d2ae046a2f8e
andreas-bylund <andreas@andreasbylund.se>
2022-11-17 22:32:05 +0100

    New feature - able to pick a todo list from a dropdown

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 json_data = null;
+data = null;
+header = null;
 
-async function onload() {
-  const res = await fetch("mock-json.json");
-  json_data = await res.json();
-  const header = document.getElementById("header");
-  header.innerText = json_data.heading;
+function onload() {
+  header = document.getElementById("header");
+  header.innerText = "Pick a file ->";
 
-  printData();
+  // printData();
+  populate_settings_menu();
 }
 
-function printData() {
+async function load_setting_file() {
+  console.log(file);
+  const res = await fetch(file);
+  json_data = await res.json();
+
+  // Update the header
+  header.innerHTML = json_data.heading;
+
+  print_data();
+}
+
+function print_data() {
   const content_div = document.getElementById("accordion");
+
+  // clear old data that may be there
+  content_div.innerHTML = '';
 
   let counter = 0;
 
@@ -62,4 +77,30 @@ function printData() {
 
     counter++;
   }
+}
+
+async function populate_settings_menu() {
+  const res = await fetch("app-settings.json");
+  data = await res.json();
+
+  const ulDropDown = document.getElementById("dropdown-menu");
+
+  for (const setting_file of data.settings_files) {
+      const menu_list_item = document.createElement("li");
+      
+      const menu_ahref_item = document.createElement("li");
+      menu_ahref_item.className = "dropdown-item";
+      menu_ahref_item.setAttribute("href", "#");
+      menu_ahref_item.setAttribute("menu_id", setting_file.id);
+      menu_ahref_item.addEventListener("click", function() {
+        file = setting_file.file_name;
+        load_setting_file();
+        console.log("Trying to open file: " + file);
+      }, false);
+      menu_ahref_item.innerText = setting_file.name
+
+      menu_list_item.appendChild(menu_ahref_item);
+      ulDropDown.appendChild(menu_list_item);
+  }
+
 }
